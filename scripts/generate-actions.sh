025e738e8508bc6c5921ea8ca99234b963b9d966
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2022-04-18 18:25:18 +0300

    Add GitHub actions

---
+++ scripts/generate-actions.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd src
+VERSIONS=$(ls -d */ | cut -f1 -d'/')
+
+for VERSION in $VERSIONS; do
+    cd $VERSION
+    DIRECTORIES=$(ls -d */ | cut -f1 -d'/')
+    for TAG in $DIRECTORIES; do
+        jq --null-input \
+          --arg php "$VERSION" \
+          --arg tag "$TAG" \
+          '{"name": ($php + "-" + $tag), "php": $php, "tag": $tag}'
+     done
+     cd ../
+done | jq -cs '
+		{
+			"fail-fast": false,
+			matrix: { include: . },
+		}
+'
