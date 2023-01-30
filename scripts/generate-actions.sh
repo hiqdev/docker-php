ebfd9ab10bdfacaf49595c3d2388d64831b08905
SilverFire - Dmytro Naumenko <d.naumenko.a@gmail.com>
2023-01-30 14:09:13 +0200

    Refactored configuration.

--- scripts/generate-actions.sh
+++ scripts/generate-actions.sh
@@ -8,14 +8,15 @@ cd src
 VERSIONS=$(ls -d */ | cut -f1 -d'/')
 
 for VERSION in $VERSIONS; do
-    # if not commit or pull request and version is older than 7.4, do not include it in the matrix
-    if [[ $EVENT_NAME != 'pull_request' ]] && [[ $EVENT_NAME != 'push' ]] && [[ $VERSION < $PERIODIC_UPDATES_MIN_VERSION ]]; then
+    # Do not include versions that are not supported by periodic updates
+    if [[ $EVENT_NAME == 'schedule' ]] && [[ $VERSION < $PERIODIC_UPDATES_MIN_VERSION ]]; then
         continue
     fi
 
     cd $VERSION
     DIRECTORIES=$(ls -d */ | cut -f1 -d'/')
     for TAG in $DIRECTORIES; do
+        # if a pull request and no files changed in the dir, do not include it in the matrix
         CHANGED_FILES_COUNT=$(echo $CHANGED_DIRS | jq ".[] | select(. | contains(\"src/$VERSION/$TAG/\"))" | wc -l)
         if [[ $EVENT_NAME == 'pull_request' ]] && [[ $CHANGED_FILES_COUNT -eq 0 ]]; then
             continue
