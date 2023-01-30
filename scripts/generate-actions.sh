3942f7ad4194c911ffc374308eefa645bdc22779
SilverFire - Dmytro Naumenko <d.naumenko.a@gmail.com>
2023-01-30 12:29:38 +0200

    Do not run cron on old PHP versions

--- scripts/generate-actions.sh
+++ scripts/generate-actions.sh
@@ -2,11 +2,17 @@
 
 EVENT_NAME=$1
 CHANGED_DIRS=$2
+PERIODIC_UPDATES_MIN_VERSION=$3
 
 cd src
 VERSIONS=$(ls -d */ | cut -f1 -d'/')
 
 for VERSION in $VERSIONS; do
+    # if not commit or pull request and version is older than 7.4, do not include it in the matrix
+    if [[ $EVENT_NAME != 'pull_request' ]] && [[ $EVENT_NAME != 'push' ]] && [[ $VERSION < $PERIODIC_UPDATES_MIN_VERSION ]]; then
+        continue
+    fi
+
     cd $VERSION
     DIRECTORIES=$(ls -d */ | cut -f1 -d'/')
     for TAG in $DIRECTORIES; do
